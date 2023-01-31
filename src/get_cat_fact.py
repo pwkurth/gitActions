d11f11da6ea34bc0337e6bd0ffabdc6e8baf9a0f
Patrick Kurth <Patrick.Kurth@PCSSoft.com>
2023-01-30 19:51:01 -0600

    Trying cat fact

---
+++ src/get_cat_fact.py
@@ -0,0 +1,17 @@
+# importing the requests library
+import requests
+
+URL = "https://catfact.ninja/fact?max_length=20"
+
+api_data = requests.get(url=URL)
+
+
+if api_data.status_code == 200:
+    try:
+        json_data = api_data.json()
+    except Exception:
+        exit('ERROR: the API_DATA provided ("{}") is not working'.format(api_data))
+else:
+    print("Error with API")
+
+print(f"::set-output cat=cat_fact::{json_data['fact']}")
